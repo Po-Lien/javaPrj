6f184209010d229b5ceac3bd10b5ac28c0068baf
Lien <a0985698048@gmail.com>
2021-05-04 22:17:49 +0800

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { MediaObserver, MediaChange } from '@angular/flex-layout';
 import { Observable, Subscription } from 'rxjs';
@@ -57,50 +56,3 @@ export class AppComponent {
   }
 
 }
-=======
-import { Component } from '@angular/core';
-import { MediaObserver, MediaChange } from '@angular/flex-layout';
-import { Observable, Subscription } from 'rxjs';
-import { AccountService } from './_services';
-import { User } from './_models';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Demo';
-  leftOpened = false;
-  mediaSub: Subscription;
-  deviceXs: boolean;
-  deviceLg: boolean;
-  deviceXl: boolean;
-  user: User;
-
-  constructor(
-    private mediaObserver: MediaObserver,
-    private accountService: AccountService
-     ){ 
-      this.accountService.user.subscribe(x => this.user = x);
-  }
-
-  toggle(){
-    this.leftOpened = !this.leftOpened;
-  }
-
-
-  ngOnInit(): void {
-    this.mediaSub = this.mediaObserver.media$.subscribe(
-      (result: MediaChange) => {
-        //console.log(result.mqAlias);
-
-        this.deviceLg = result.mqAlias === 'lg' ? true : false;
-        this.deviceXl = result.mqAlias === 'xl' ? true : false;
-        this.deviceXs = result.mqAlias === 'xs' ? true : false;
-      }
-    );
-  }
-
-}
->>>>>>> ddc21a131312db80ff3c782a51e13cf9724c86fb
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -56,59 +55,3 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-=======
-import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ContainerComponent } from './container/container.component';
-import { MessageAreaComponent } from './container/message-area/message-area.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { ListRoutingModule } from './container/list/list-routing.module';
-
-import { ShareMaterialModule } from './share-material/share-material.module';
-import { HomeComponent } from './home/home.component';
-import { TripsComponent, DialogSetScheduleDialog } from './trips/trips.component';
-import { AlertComponent } from './account/alert/alert.component';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-
-import { JwtInterceptor, ErrorInterceptor } from './_helpers';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ContainerComponent,
-    MessageAreaComponent,
-    HomeComponent,
-    TripsComponent,
-    DialogSetScheduleDialog,
-    AlertComponent,
-  ],
-  imports: [
-    FlexLayoutModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    ShareMaterialModule,
-    BrowserAnimationsModule,
-    BrowserModule,
-    CommonModule,
-    FormsModule,
-    ListRoutingModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-  ],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
->>>>>>> ddc21a131312db80ff3c782a51e13cf9724c86fb
